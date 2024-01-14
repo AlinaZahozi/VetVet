082dc9a090eb129384afe77d0dd0012a7a60b798
alina_z <alinush0zahozi@gmail.com>
2024-01-14 12:35:34 +0200

    backup

---
+++ screens/index.js
@@ -0,0 +1,15 @@
+import Create from "./Create";
+import Messages from "./Messages";
+import VetProfile from "./VetProfile";
+import EditVetProfile from "./EditVetProfile";
+import Settings from "./Settings";
+import Home from "./Home";
+
+export {
+    Create,
+    Messages,
+    VetProfile,
+    EditVetProfile,
+    Settings,
+    Home
+}
